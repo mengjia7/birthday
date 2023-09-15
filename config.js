1c651cc0c39962b4d80ac10bd3e6b64705fa5947
mengjia7 <128562349+mengjia7@users.noreply.github.com>
2023-09-15 23:37:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,10 @@ var config = {
         "今天是你的生日",
         "这是我们在一起的",
         "第一个生日哦",
-        "今年第一目标",
-        "要把我家可爱宝贝喂胖一点点",
-        "然后抱走",
+        "生日快乐🎂",
+        "不止今天哦",
+        "以后每一天都要快乐",
+        "我会一直陪着你 爱你哦",
         "哈哈~~",
     ],
     /**
@@ -23,18 +24,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "心爱的小可爱": "./imgs/xningbaoer.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
+        turn_on: "点这里开始～",
+        play: "音乐！走～",
+        bannar_coming: "颜色～",
+        balloons_flying: "好像少点东西哦",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        wish_message: "生日快乐 我滴宁宝儿",
+        story: "还有一些话～",
     }
 };
